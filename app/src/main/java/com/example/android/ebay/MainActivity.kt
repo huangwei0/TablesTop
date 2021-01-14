17a2d724572403029b34568b1d0ccb8f5612aeb6
huangwei0 <40774443+huangwei0@users.noreply.github.com>
2021-01-13 20:53:18 -0800

    common

---
+++ app/src/main/java/com/example/android/ebay/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.android.ebay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val profileFragment = profileFragment()
+        val socialFragment = socialFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_social ->makeCurrentFragment(socialFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer,fragment )
+            commit()
+        }
+}
